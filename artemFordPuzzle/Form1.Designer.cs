8c6972ed71d0bc0c332d540c25d156ab36520674
Opilane TTHK <opilane@tthk.ee>
2020-01-15 15:49:50 +0200

    first commit

---
+++ artemFordPuzzle/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace artem_figures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = global::artemFordPuzzle.Properties.Resources.car;
+            this.pictureBox1.Image = global::artemFordPuzzle.Properties.Resources.car;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1015, 433);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(717, 275);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(156, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(414, 247);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(596, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1039, 467);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ artemFordPuzzle/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace artem_figures
+{
+    public partial class Form1 : Form
+    {
+        bool SquareleClicked, SquareleClicked2, DoorClicked, DoorClicked2 = false;
+        bool StatusDone, StatusDone2, StatusDone3, StatusDone4 = false;
+        int  SquareX, SquareY, SquareX2, SquareY2, DoorX, DoorY, DoorX2, DoorY2 = 0;
+        int X, Y, dX, dY;
+        int LastClicked = 0;
+        Image img,img2, img3;
+
+        Rectangle door = new Rectangle(220, 10, 200, 143);
+        Rectangle door2 = new Rectangle(220, 10, 220, 143);
+        Rectangle square = new Rectangle(380, 10, 150, 150);
+        Rectangle square2 = new Rectangle(380, 10, 150, 150);
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.MouseDown += PictureBox1_MouseDown;
+            pictureBox1.MouseUp += PictureBox1_MouseUp;
+            pictureBox1.MouseMove += PictureBox1_MouseMove;
+
+            img = Image.FromFile("C:/Users/opilane/projects/artemFordPuzzle/artemFordPuzzle/images/carwheel.png");
+            img2 = Image.FromFile("C:/Users/opilane/projects/artemFordPuzzle/artemFordPuzzle/images/door.png");
+            img3 = Image.FromFile("C:/Users/opilane/projects/artemFordPuzzle/artemFordPuzzle/images/door2.png");
+
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (StatusDone == false)
+            {
+                if (SquareleClicked)
+                {
+                    square.X = e.X - SquareX;
+                    square.Y = e.Y - SquareY;
+                }
+            }
+            if (StatusDone2 == false)
+            {
+                if (SquareleClicked2)
+                {
+                    square2.X = e.X - SquareX2;
+                    square2.Y = e.Y - SquareY2;
+                }
+            }
+            if (StatusDone3 == false)
+            {
+                if (DoorClicked)
+                {
+                    door.X = e.X - DoorX;
+                    door.Y = e.Y - DoorY;
+                }
+            }
+            if (StatusDone4 == false)
+            {
+                if (DoorClicked2)
+                {
+                    door2.X = e.X - DoorX2;
+                    door2.Y = e.Y - DoorY2;
+                }
+            }
+            pictureBox1.Invalidate();
+
+
+
+
+        }
+
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (((label1.Location.X < square.X + square.Width) && (label1.Location.X > square.X))
+              && ((label1.Location.Y < square.Y + square.Height) && (label1.Location.Y > square.Y)))
+            {
+                StatusDone = true;
+                square.X = 687;
+                square.Y = 230;
+
+            }
+            if (((label2.Location.X < square2.X + square2.Width) && (label2.Location.X > square2.X))
+              && ((label2.Location.Y < square2.Y + square2.Height) && (label2.Location.Y > square2.Y)))
+            {
+                StatusDone2 = true;
+                square2.X = 120;
+                square2.Y = 225;
+
+            }
+            if (((label3.Location.X < door.X + door.Width) && (label3.Location.X > door.X))
+              && ((label3.Location.Y < door.Y + door.Height) && (label3.Location.Y > door.Y)))
+            {
+                StatusDone3 = true;
+                door.X = 325;
+                door.Y = 167;
+
+            }
+            if (((label4.Location.X < door2.X + door2.Width) && (label4.Location.X > door2.X))
+              && ((label4.Location.Y < door2.Y + door2.Height) && (label4.Location.Y > door2.Y)))
+            {
+                StatusDone4 = true;
+                door2.X = 522;
+                door2.Y = 164;
+
+            }
+
+
+
+            if (SquareleClicked)
+            {
+                LastClicked = 1;
+                X = square.X;
+                Y = square.Y;
+            }
+            else if (SquareleClicked2)
+            {
+                LastClicked = 2;
+                X = square2.X;
+                Y = square2.Y;
+            }
+            else if (DoorClicked)
+            {
+                LastClicked = 3;
+                X = door.X;
+                Y = door.Y;
+            }
+            else if (DoorClicked2)
+            {
+                LastClicked = 4;
+                X = door2.X;
+                Y = door2.Y;
+            }
+            SquareleClicked = false;
+            SquareleClicked2 = false;
+            DoorClicked = false;
+            DoorClicked2 = false;
+
+
+        }
+
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+            if ((e.X < square.X + square.Width) && (e.X > square.X))
+            {
+                if ((e.Y < square.Y + square.Height) && (e.Y > square.Y))
+                {
+                    SquareleClicked = true;
+
+                    SquareX = e.X - square.X;
+                    SquareY = e.Y - square.Y;
+                }
+            }
+            else if ((e.X < square2.X + square2.Width) && (e.X > square2.X))
+            {
+                if ((e.Y < square2.Y + square2.Height) && (e.Y > square2.Y))
+                {
+                    SquareleClicked2 = true;
+
+                    SquareX2 = e.X - square2.X;
+                    SquareY2 = e.Y - square2.Y;
+                }
+            }
+            else if ((e.X < door.X + door.Width) && (e.X > door.X))
+            {
+                if ((e.Y < door.Y + door.Height) && (e.Y > door.Y))
+                {
+                    DoorClicked = true;
+
+                    DoorX = e.X - door.X;
+                    DoorY = e.Y - door.Y;
+                }
+            }
+            else if ((e.X < door2.X + door2.Width) && (e.X > door2.X))
+            {
+                if ((e.Y < door2.Y + door2.Height) && (e.Y > door2.Y))
+                {
+                    DoorClicked2 = true;
+
+                    DoorX2 = e.X - door2.X;
+                    DoorY2 = e.Y - door2.Y;
+                }
+            }
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_Click(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(img, square);
+            e.Graphics.DrawImage(img, square2);
+            e.Graphics.DrawImage(img2, door);
+            e.Graphics.DrawImage(img3, door2);
+
+        }
+    }
+}
